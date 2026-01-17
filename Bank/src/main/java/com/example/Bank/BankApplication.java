48861fd8e5759917c12a649a64f992083fbccaa8
KrzysztofL05 <30lipiec03@gmail.com>
2026-01-17 14:24:48 +0100

    BankApp psuh

--- Bank/src/main/java/com/example/Bank/BankApplication.java
+++ Bank/src/main/java/com/example/Bank/BankApplication.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankApplication {
 
+	public BankApplication(BankService bankService) {
+
+		bankService.registerClient(1, 1000);
+		bankService.registerClient(1,2);
+		bankService.registerClient(2, 200);
+		bankService.registerClient(3, 450);
+
+		System.out.println(bankService.transfer(1, 300).getStatus());
+		System.out.println(bankService.transfer(2, 500).getStatus());
+		System.out.println(bankService.transfer(3, 450).getStatus());
+		System.out.println(bankService.deposit(2, 400).getNewBalance());
+		System.out.println(bankService.deposit(3, 2137).getStatus());
+		System.out.println(bankService.getClientData(1).getNewBalance());
+		System.out.println(bankService.getClientData(3).getNewBalance());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
-
 }
+
